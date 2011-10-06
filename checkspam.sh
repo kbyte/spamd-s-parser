5dffc049a79e07c8347e6bc34c77e61f3444bdc4
Kbyte <ptrschz@gmail.com>
2011-10-06 09:34:27 -0500

    Cambios: -Formateo de la salida en pantalla. -Opción "-h" para ayuda. -Opción <logfile path> para indicar una ubicación diferente a la de default del spamd.log

--- checkspam.sh
+++ checkspam.sh
@@ -1,36 +1,64 @@
 #!/bin/bash
 #checkspam.sh parse the spamd.log to print a report whit spamers domain and IP Address
-#created by Kbyte <ptrschz@gmail.com>
 
-# Generating files to parse
-echo "Parsing spamd.log file..."
+cd /home/psanchez/bin/
+clear
 
-#check in spamd.log all entries identified as spam and create idspam.txt file
-grep "result: Y" /var/log/spamassassin/spamd.log > idspam.txt
+to_file=$1
 
-#parsing all that who has domain identified
+if [ "$to_file" == "-h" ]
+ then
+  echo "Checkspam.sh homemade parser to spamd.log"
+  echo "========================================="
+  echo " "
+  echo "Usage: checkspam.sh <logfile path> to indicate a specific spamd.log path"
+  echo "Usage: checkspam.sh to get the default spamd.log path (/var/log/spamassassin/spamd.log)"
+  echo "Usage: checkspam.sh to show this screen"
+  echo " "
+  exit 0
+fi
+
+if test -z $to_file 
+ then 
+  to_file="/var/log/spamassassin/spamd.log"
+ else
+  if ! test -e $to_file
+   then
+    echo "checkspam (can not get access to $to_file or does not exist the log, try again...)"
+    exit 0
+  fi
+fi
+
+echo -e "checkspamd (logfile: $to_file)...\n"
+
+grep "result: Y" $to_file > idspam.txt
 if grep -v -e "unknown" idspam.txt > toparse.txt
  then
   i=1
   while read line 
   do
-   #extract date
    to_date=$(head -$i /home/psanchez/bin/toparse.txt | cut -d "[" -f 1 | tail -1) 
-   #extract domain
-   to_extract=$(head -$i /home/psanchez/bin/toparse.txt | cut -d "@" -f 2 | cut -d ">" -f 1 | tail -1)
-   #get the ip address by nslookup
-   to_address=$(nslookup $to_extract | grep "Address: " | tail -1)
-   #generating gral_spam.txt file as report
-   echo "$to_date| $to_extract | $to_address" >> gral_spam.txt
-   let i=i+1
+   to_extract=$(head -$i /home/psanchez/bin/toparse.txt | cut -d "<" -f 2 | cut -d "@" -f 2 | cut -d ">" -f 1 | tail -1)
+   to_address=$(nslookup $to_extract | grep "Address: " | cut -d ":" -f 2 | tail -1)
+   
+   if test -z $to_address ; then to_address=" unknown ip" ; fi
+
+   echo -e "\e[1;34m$to_date\e[0m \e[1;33m$to_extract\e[0m (\e[1;31m$to_address \e[0m)" >> gral_spam.txt
+  let i=i+1
   done < toparse.txt
 fi
-clear
-#show on screen the report eliminating repeated entries
-cat gral_spam.txt | uniq
+
+if test -s gral_spam.txt 
+ then 
+  echo "Date                      Identified Domain and IP Address (Only if is available)"
+  echo "================================================================================="
+  echo " "
+  cat gral_spam.txt | uniq
+ else
+  echo "No Spam has been identified yet..."
+fi
 echo " "
 echo " "
 echo "Parse has been done..."
-#removing generated .txt files
 rm *.txt
 exit 0
